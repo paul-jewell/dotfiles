9bfe22767cc7bbab05516710de6615139a80c055
Paul Jewell <paul@teulu.org>
2021-01-22 22:11:58 +0000

    Setup whole dotfile structure
    Lots of refactoring required, but I wanted to get to a position where
    I can configure zeus (guix laptop) properly.

---
+++ .config/guix/manifests/code.scm
@@ -0,0 +1,34 @@
+;; Various developer tools that I use.  These might be split out into
+;; platform-specific manifests at some point.
+
+(specifications->manifest
+ '(;; C/C++
+   "gcc-toolchain"
+   "make"
+   "pkg-config"
+   "texinfo"
+   "llvm"
+   "lld"
+   "clang"
+
+   ;; Python
+   "python3"
+
+   ;; Docker
+   "docker-cli"
+
+   ;; Java
+   "icedtea"
+
+   ;; SDL
+   "glu"
+   "glfw"
+   "sdl2"
+   "sdl2-image"
+   "sdl2-mixer"
+   "sdl2-gfx"
+   "sdl2-ttf"
+
+   "curl"
+   "virt-manager"))
+   ;; "glibc" ;; For ldd
---
+++ .config/guix/manifests/music.scm
@@ -0,0 +1,34 @@
+;; Music creation tools
+
+(specifications->manifest
+ '(;; JACK tools
+   "jack"
+   "jack2"
+   "jack-keyboard"
+   "qjackctl"
+   "patchage"
+
+   ;; DAWs
+   "ardour"
+   "zrythm"
+
+   ;; Guitar
+   ;; "guitarix"
+   ;; "guitarix-lv2"
+
+   ;; Effects
+   "calf"
+   "g2reverb"
+   "dragonfly-reverb"
+   "wolf-shaper"
+
+   ;; Synths
+   "helm"
+   "amsynth"
+   "avldrums-lv2"
+   "geonkick"
+   "fluidsynth"
+   "zynaddsubfx"
+
+   ;; Mixing Tools
+   "wolf-spectrum"))
