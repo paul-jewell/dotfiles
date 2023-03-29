30241d56fd5fda0bf0f877cdb773951e0972902f
Paul Jewell <paul@teulu.org>
2023-03-29 17:57:49 +0100

    Fix typo.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -135,7 +135,7 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
+#  };
 
   services.mpd = {
       enable = true;
