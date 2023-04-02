2b8caede3e3c4225b0ca772e473d7879ef3a474a
Paul Jewell <paul@teulu.org>
2023-04-02 17:32:26 +0100

    WIP - debugging

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,13 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixoS/nixos-hardware";
     home-manager = {
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager }: 
+  outputs = { self, nixpkgs, nixos-hardware, home-manager }: 
     let
       user = "paul";
       name = "Paul Jewell";
@@ -26,6 +27,7 @@
           inherit system;
           modules = [
             ./configuration.nix
+            nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen3
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
