d15af054abbd593702e912d968904a13a0ccf72a
Paul Jewell <paul@teulu.org>
2023-04-05 09:05:31 +0100

    Remove picom - debugging amdgpu issues

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -11,12 +11,13 @@ in
       allowUnsupportedSystem = true;
     };
 
-    overlays =
-      # Apply each overlay found in the /overlays directory
-      let path = ../overlays; in with builtins;
-      map (n: import (path + ("/" + n)))
-          (filter (n: match ".*\\.nix" n != null ||
-                      pathExists (path + ("/" + n + "/default.nix")))
-                  (attrNames (readDir path)));
+  #   overlays =
+  #     # Apply each overlay found in the /overlays directory
+  #     let path = ../overlays; in with builtins;
+  #     map (n: import (path + ("/" + n)))
+  #         (filter (n: match ".*\\.nix" n != null ||
+  #                     pathExists (path + ("/" + n + "/default.nix")))
+  #                 (attrNames (readDir path)));
   };
 }
+        
