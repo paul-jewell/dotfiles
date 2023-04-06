66096177977309295d33fe0f5ca57ebf82249835
Paul Jewell <paul@teulu.org>
2023-04-06 06:31:36 +0100

    Added isolde directories/files.

---
+++ nixos-new/home/isolde/dev.nix
@@ -0,0 +1,8 @@
+{config, ...}: {
+# TODO: Understand this, and add my gpg keys?
+  xdg.configFile."pam-gnupg".text = ''
+    ${config.programs.gpg.homedir}
+    2622167BDE636A248CE883080EE77D752284FDF4
+    BF9C6D61344A624956EB93594834D4D2AF5BD8C1
+  '';
+}
