aa35d3651b72dd4d039561918b396540c13055bf
Paul Jewell <paul@teulu.org>
2023-04-13 17:13:35 +0100

    Removed mangohud

--- nixos/home/common/default.nix
+++ nixos/home/common/default.nix
@@ -19,8 +19,8 @@
     ./polybar.nix
     ./dunst.nix
     ./texlive.nix
-#    ./screenshot-bash.nix
-#    ./sway
+    #    ./screenshot-bash.nix
+    #    ./sway
   ];
 
   home.packages = with pkgs; [
@@ -30,10 +30,10 @@
     bat
     ledger
 
-#    feh
+    #    feh
 
     gcc
-    gh 
+    gh
 
     keepassxc
     neofetch
@@ -95,31 +95,7 @@
   programs.firefox.enable = true;
   programs.browserpass.enable = true;
 
-#  services.syncthing.enable = true;
-  programs.mangohud = {
-    enable = true;
-    settings = {
-      fps_limit = "150,60,0";
-      vsync = 1;
-      cpu_stats = true;
-      cpu_temp = true;
-      gpu_stats = true;
-      gpu_temp = true;
-      vulkan_driver = true;
-      fps = true;
-      frametime = true;
-      frame_timing = true;
-      enableSessionWide = true;
-      font_size = 24;
-      position = "top-left";
-      engine_version = true;
-      wine = true;
-      no_display = true;
-      background_alpha = "0.5";
-      toggle_hud = "Shift_R+F12";
-      toggle_fps_limit = "Shift_R+F1";
-    };
-  };
+  #  services.syncthing.enable = true;
 
   programs.obs-studio = {
     enable = true;
