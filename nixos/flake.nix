2963a4e645681536a1d0f6dd42e53980f375d6b7
Paul Jewell <paul@teulu.org>
2023-04-21 06:37:33 +0100

    Updated flake description field
    Also formatting fixed.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration of Jane Doe";
+  description = "System configuration flake - Paul Jewell";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -8,16 +8,16 @@
     # My fork - for development of x390 hardware files
     nixos-hardware.url = "github:paul-jewell/nixos-hardware/lenovo-x390";
     pre-commit-hooks = {
-       url = "github:cachix/pre-commit-hooks.nix";
-       inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     agenix = {
-        url = "github:ryantm/agenix";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     screenshot-bash = {
@@ -43,7 +43,7 @@
       checks = {
         pre-commit-check = pre-commit-hooks.lib.${system}.run {
           src = ./.;
-          hooks = {alejandra.enable = true; };
+          hooks = {alejandra.enable = true;};
         };
       };
       devShells.default = pkgs.mkShell {
@@ -61,9 +61,9 @@
         config = {allowUnfree = true;};
         overlays = [screenshot-bash.overlays.default self.overlays.default];
       };
-      
+
       username = "paul";
-      
+
       mkHost = {
         hostName,
         system,
@@ -73,47 +73,48 @@
         ${hostName} = nixpkgs.lib.nixosSystem {
           inherit system;
           inherit pkgs;
-          modules = [
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-               home-manager.sharedModules = pkgs.lib.attrValues self.hmModules;
-            }
-            agenix.nixosModules.age
-            ./host/common
-            ./host/${hostName}
-            ({lib, ...}: {networking.hostName = lib.mkDefault hostName;})
-            
-            (import ./home username)
-          ]
-          ++ modules;
+          modules =
+            [
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.sharedModules = pkgs.lib.attrValues self.hmModules;
+              }
+              agenix.nixosModules.age
+              ./host/common
+              ./host/${hostName}
+              ({lib, ...}: {networking.hostName = lib.mkDefault hostName;})
+
+              (import ./home username)
+            ]
+            ++ modules;
+        };
       };
+    in {
+      nixosConfigurations =
+        (mkHost {
+          inherit system;
+          inherit pkgs;
+          hostName = "isolde";
+          modules = [nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen3];
+        })
+        // (mkHost {
+          inherit system;
+          inherit pkgs;
+          hostName = "gandalf";
+          # modules = [];
+          modules = [nixos-hardware.nixosModules.lenovo-thinkpad-x390];
+        });
+    })
+    // {
+      hmModules = {
+        beets = import ./modules/hm/beets.nix;
+        catppuccin = import ./modules/hm/catppuccin.nix;
+        fish-theme = import ./modules/hm/fish-theme.nix;
+        fuzzel = import ./modules/hm/fuzzel.nix;
+        pipewire = import ./modules/hm/pipewire.nix;
+      };
+      overlays.default = import ./pkgs;
     };
-  in {
-    nixosConfigurations =
-      (mkHost {
-        inherit system;
-        inherit pkgs;
-        hostName = "isolde";
-        modules = [nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen3];
-      })
-      // (mkHost {
-        inherit system;
-        inherit pkgs;
-        hostName = "gandalf";
-	      # modules = [];
-        modules = [nixos-hardware.nixosModules.lenovo-thinkpad-x390];
-      });
-  })
-  // {
-    hmModules = {
-      beets      = import ./modules/hm/beets.nix;
-      catppuccin = import ./modules/hm/catppuccin.nix;
-      fish-theme = import ./modules/hm/fish-theme.nix;
-      fuzzel     = import ./modules/hm/fuzzel.nix;
-      pipewire   = import ./modules/hm/pipewire.nix;
-    };
-    overlays.default = import ./pkgs;
-  };
 }
