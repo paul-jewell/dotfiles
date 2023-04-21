3eec3e4d46e80ad4254098e9d05900c07c4d301d
Paul Jewell <paul@teulu.org>
2023-04-21 07:56:47 +0100

    Re-introduced limit to system builds

--- nixos/host/isolde/boot.nix
+++ nixos/host/isolde/boot.nix
@@ -6,7 +6,7 @@
   boot = {
     loader = {
       systemd-boot.enable = true;
-#      systemd-boot.configurationLimit = 10;
+      systemd-boot.configurationLimit = 10;
       efi.canTouchEfiVariables = true;
       efi.efiSysMountPoint = "/boot/efi";
     };
@@ -20,5 +20,3 @@
   };
   hardware.enableRedistributableFirmware = true;
 }
-
-      
