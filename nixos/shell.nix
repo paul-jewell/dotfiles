e327bc41d591044270410ea77bd4526b6704e553
Paul Jewell <paul@teulu.org>
2023-05-02 12:42:50 +0100

    Moved home manager modules

---
+++ nixos/shell.nix
@@ -0,0 +1,35 @@
+# Shell for bootstrapping flake-enabled nix and other tooling
+{ pkgs ? # If pkgs is not defined, instanciate nixpkgs from locked commit
+  let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+    import nixpkgs { overlays = [ ]; }
+, ...
+}: {
+  default = pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+    nativeBuildInputs = with pkgs; [
+      nix
+      home-manager
+      git
+
+      agentix
+      sops
+      gnupg
+      age
+    ];
+  };
+}
+
+  # Original devshell had shell hook for pre-commit check and used
+  # alejandra for formatting.
+  #     inherit (self.checks.${system}.pre-commit-check) shellHook;
+  #   packages = with pkgs; [
+  #     alejandra
+  #     agenix.packages.${system}.agenix
+  #   ];
+  # };
