147678a832b727e32515432f3290d5be9416c0bc
Paul Jewell <paul@teulu.org>
2024-11-26 16:43:14 +0000

    Created base.scm, and modified pluto.scm to match

---
+++ paulj/config/systems/base.scm
@@ -0,0 +1,119 @@
+(define-module (paulj systems base)
+  #:use-module (gnu)
+  #:use-module (gnu system)
+  #:use-module (gnu system nss)
+  #:use=module (gnu system setuid)
+  #:export (system-config))
+
+(use-service-modules guix admin sysctl pm avahi dbus cups desktop linux
+                     mcron networking xorg ssh audio virtualization)
+
+(use-package-modules audio video nfs certs shells ssh linux bash emacs
+                     networking wm fonts libusb cups freedesktop file-systems
+                     version-control package-management vim)
+
+(define-public base-operating-system
+  (operating-system
+   (host-name "base-system")
+   (timezone "Europe/London")
+   (locale "en_GB.utf8")
+   (keyboard-layout (keyboard-layout "gb" "extd"))
+   
+   ;; Use UEFI version of GRUB with EFI System
+   ;; Partition mounted on /boot/efi 
+   (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets (list "/boot/efi"))
+                (keyboard-layout keyboard-layout)))
+
+   (file-systems (cons*
+                  (file-system
+                   (mount-point "/tmp")
+                   (device "none")
+                   (type "tmpfs")
+                   (check? #f))
+                  %base-file-systems))
+
+   (users (cons* (user-account
+                  (name "paul")
+                  (comment "Paul Jewell")
+                  (group "users")
+                  (home-directory "/home/paul")
+                  (supplementary-groups '("wheel" "netdev" "lp" "audio" "video")))
+                 %base-user-accounts))
+
+   (packages (cons* nss-certs
+                    emacs-no-x-toolkit
+                    exfat-utils
+                    fuse-exfat
+                    git
+                    gvfs
+                    stow
+                    vim
+                    %base-packages))
+   
+   ;; Below is the list of system services.  To search for available
+   ;; services, run 'guix system search KEYWORD' in a terminal.
+   (services
+    (append (list (service xfce-desktop-service-type)
+                  
+                  ;; To configure OpenSSH, pass an 'openssh-configuration'
+                  ;; record as a second argument to 'service' below.
+                  (service openssh-service-type)
+                  (service cups-service-type)
+                  (set-xorg-configuration
+                   (xorg-configuration (keyboard-layout keyboard-layout))))
+            
+            ;; This is the default list of services we
+            ;; are appending to.
+            %desktop-services))
+   ;; Allow resolution of '.local' host names with mDNS
+   (name-service-switch %mdns-host-lookup-nss)))
+
+(define* (system-config #:key system home)
+  (operating-system
+   (inherit system)
+   (timezone "Europe/London")
+   (locale "en_GB.utf8")
+
+   (keyboard-layout (keyboard-layout "gb" "extd"))
+
+   (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets '("/boot/efi"))
+                (keyboard-layout keyboard-layout)))
+
+   (users (cons* (user-account
+                  (name "paul")
+                  (comment "Paul Jewell")
+                  (group "users")
+                  (home-directory "/home/paul")
+                  (supplementary-groups '("wheel" "netdev" "lp" "audio" "video")))
+                 %base-user-accounts))
+
+   (packages (cons* nss-certs
+                    emacs-no-x-toolkit
+                    exfat-utils
+                    fuse-exfat
+                    git
+                    gvfs
+                    stow
+                    vim
+                    %base-packages))
+
+   (services
+    (append (list (service xfce-desktop-service-type)
+                  
+                  ;; To configure OpenSSH, pass an 'openssh-configuration'
+                  ;; record as a second argument to 'service' below.
+                  (service openssh-service-type)
+                  (service cups-service-type)
+                  (set-xorg-configuration
+                   (xorg-configuration (keyboard-layout keyboard-layout))))
+            
+            ;; This is the default list of services we
+            ;; are appending to.
+            %desktop-services))
+
+   ;; Allow resolution of '.local' host names with mDNS
+   (name-service-switch %mdns-host-lookup-nss)))
--- paulj/config/systems/pluto.scm
+++ paulj/config/systems/pluto.scm
@@ -1,4 +1,5 @@
 (define-module (paulj systems pluto)
+  #:use-module (paulj systems base)
   #:use-module (gnu))
 
 (use-service-modules cups desktop networking ssh xorg)
@@ -6,45 +7,13 @@
 (system-config
  #:system
  (operating-system
-  (locale "en_GB.utf8")
-  (timezone "Europe/London")
-  (keyboard-layout (keyboard-layout "gb" "extd"))
   (host-name "pluto")
 
-  ;; The list of user accounts ('root' is implicit).
-  (users (cons* (user-account
-                 (name "paul")
-                 (comment "Paul Jewell")
-                 (group "users")
-                 (home-directory "/home/paul")
-                 (supplementary-groups '("wheel" "netdev" "audio" "video")))
-                %base-user-accounts))
-
-  ;; Packages installed system-wide.  Users can also install packages
-  ;; under their own account: use 'guix search KEYWORD' to search
-  ;; for packages and 'guix install PACKAGE' to install a package.
-  (packages (append (list (specification->package "nss-certs"))
-                    %base-packages))
-
-  ;; Below is the list of system services.  To search for available
-  ;; services, run 'guix system search KEYWORD' in a terminal.
-  (services
-   (append (list (service xfce-desktop-service-type)
-
-                 ;; To configure OpenSSH, pass an 'openssh-configuration'
-                 ;; record as a second argument to 'service' below.
-                 (service openssh-service-type)
-                 (service cups-service-type)
-                 (set-xorg-configuration
-                  (xorg-configuration (keyboard-layout keyboard-layout))))
-
-           ;; This is the default list of services we
-           ;; are appending to.
-           %desktop-services))
   (bootloader (bootloader-configuration
                (bootloader grub-efi-bootloader)
                (targets (list "/boot/efi"))
                (keyboard-layout keyboard-layout)))
+
   (swap-devices (list (swap-space
                        (target (uuid
                                 "755a3ec1-9552-41ea-b1ab-9a9298aab04a")))))
@@ -59,4 +28,5 @@
                         (mount-point "/boot/efi")
                         (device (uuid "7557-8745"
                                       'fat32))
-                        (type "vfat")) %base-file-systems))))
+                        (type "vfat")) %base-file-systems))
+  (services (list))))
