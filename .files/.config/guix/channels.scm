6230197a50f1b80bee891cb8b3837b6a97e2b025
Paul Jewell <paul@teulu.org>
2024-12-02 16:45:27 +0000

    Added channel files

---
+++ .files/.config/guix/channels.scm
@@ -0,0 +1,22 @@
+(list (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "d9f5fc57b3d971cdbafbffb62119142419d61d3f")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "047967c42f237695d5af2af53321c70f157685a3")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
