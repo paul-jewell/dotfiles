ed1cc7a4e1bb42d0ce273ed802209794e5b94fea
Paul Jewell <paul@teulu.org>
2024-12-31 08:40:49 +0000

    Working on disk image creation for installation.
    - Added helix via saayix channel
    - Having issues with wifi card on X270 - WIP

--- .files/.config/guix/channels.scm
+++ .files/.config/guix/channels.scm
@@ -20,4 +20,13 @@
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+       (name 'saayix)
+       (url "https://codeberg.org/look/saayix.git")
+       (branch "entropy")
+       (introduction
+        (make-channel-introduction
+         "12540f593092e9a177eb8a974a57bb4892327752"
+         (openpgp-fingerprint
+          "3FFA 7335 973E 0A49 47FC  0A8C 38D5 96BE 07D3 34AB")))))
