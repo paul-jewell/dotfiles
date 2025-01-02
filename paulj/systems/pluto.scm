cc7adbf959fb85dbf0a85ad5e9dfba88da29548f
Paul Jewell <paul@teulu.org>
2025-01-02 10:37:10 +0000

    Moved file system definitions to separate module

--- paulj/systems/pluto.scm
+++ paulj/systems/pluto.scm
@@ -1,16 +1,15 @@
 (define-module (paulj systems pluto)
   #:use-module (paulj systems base)
   #:use-module (paulj systems common)
+  #:use-module (paulj systems pluto file-systems)
   #:use-module (paulj home-services video)
   #:use-module (paulj home-services finance)
   #:use-module (gnu)
   #:use-module (gnu home)
   #:use-module (gnu home services sound)
-  #:use-module (gnu packages file-systems)
   #:use-module (gnu services)
   #:use-module (gnu system)
   #:use-module (gnu system uuid)
-  #:use-module (gnu system file-systems)
   #:use-module (gnu system))
 
 (system-config
@@ -36,15 +35,5 @@
                        (target (uuid
                                 "755a3ec1-9552-41ea-b1ab-9a9298aab04a")))))
 
-  (file-systems (cons* (file-system
-                        (mount-point "/")
-                        (device (uuid
-                                 "a5e463ab-8489-42e2-bf20-c3fad9423ea7"
-                                 'xfs))
-                        (type "xfs"))
-                       (file-system
-                        (mount-point "/boot/efi")
-                        (device (uuid "7557-8745"
-                                      'fat32))
-                        (type "vfat")) %base-file-systems))
+  (file-systems %pluto-file-systems)
   (services (list))))
