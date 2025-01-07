be3ac8239a05b633ea244ed1f1b7e6521a7849f5
Paul Jewell <paul@teulu.org>
2025-01-07 21:26:41 +0000

    Moved from ext4 to btrfs ephemeral file systems

--- paulj/systems/claudio/file-systems.scm
+++ paulj/systems/claudio/file-systems.scm
@@ -1,66 +1,165 @@
 (define-module (paulj systems claudio file-systems)
   #:use-module (gnu system file-systems)
-  #:export (%ext4-file-systems))
+  #:export (%btrfs-ephemeral-file-systems))
 
-(define guix-root-part
-  (file-system-label "guix-root"))
+(define guix-part
+  (file-system-label "guix"))
 
-(define guix-home-part
-  (file-system-label "guix-home"))
-
-(define guix-boot-part
+(define efi-part
   (file-system-label "guix-boot"))
 
 (define root
   (file-system
-   (device guix-root-part)
+   (device "none")
    (mount-point "/")
-   (type "ext4")))
+   (type "tmpfs")
+   (check? #f)
+   (needed-for-boot? #t)
+   (options "mode=0755")))
 
 (define home
   (file-system
-   (device guix-home-part)
+   (device guix-part)
+   (type "btrfs")
    (mount-point "/home")
-   (type "ext4")))
+   (flags '(no-atime))
+   (options "subvol=@home,discard=async,ssd")))
+
+(define root-user
+  (file-system
+   (device guix-part)
+   (type "btrfs")
+   (mount-point "/root")
+   (flags '(no-atime))
+   (options "subvol=@root,discard=async,ssd")))
 
 (define boot
   (file-system
-   (device guix-boot-part)
-   (mount-point "/boot/efi")
-   (type "vfat")))
+   (device guix-part)
+   (mount-point "/boot")
+   (type "btrfs")
+   (check? #f)
+   (needed-for-boot? #t)
+   (flags '(no-atime))
+   (options "subvol=@boot,discard=async,ssd")))
+
+(define boot-efi
+  (file-system
+   (device efi-part)
+   (type "vfat")
+   (mount-point "/boot/efi")))
 
 (define tmp
   (file-system
    (device "none")
    (mount-point "/tmp")
    (type "tmpfs")
-   (check? #f)))
+   (check? #f)
+   (needed-for-boot? #f)))
 
 (define run
   (file-system
    (device "none")
    (mount-point "/run")
    (type "tmpfs")
-   (options "mode=0755")
    (needed-for-boot? #t)
-   (check? #f)))
+   (check? #f)
+   (options "mode=0755")))
 
 (define var-run
   (file-system
    (device "none")
    (mount-point "/var/run")
    (type "tmpfs")
-   (options "mode=0755")
    (needed-for-boot? #t)
-   (check? #f)))
+   (check? #f)
+   (options "mode=0755")))
 
-(define %ext4-file-systems
+(define var-log
+  (file-system
+   (device guix-part)
+   (type "btrfs2")
+   (mount-point "/var/log")
+   (check? #f)
+   (needed-for-boot? #t)
+   (flags '(no-atime))
+   (options "compress=zstd,subvol=@var/log,ssd")))
+
+(define var-lib
+  (file-system
+   (device guix-part)
+   (type "btrfs2")
+   (mount-point "/var/lib")
+   (needed-for-boot? #t)
+   (flags '(no-atime))
+   (options "compress=zstd,subvol=@var/lib,ssd")))
+
+(define var-guix
+  (file-system
+   (device guix-part)
+   (type "btrfs2")
+   (mount-point "/var/guix")
+   (needed-for-boot? #t)
+   (flags '(no-atime))
+   (options "compress=zstd,subvol=@var/guix,ssd")))
+
+(define gnu-store
+  (file-system
+   (device guix-part)
+   (type "btrfs2")
+   (mount-point "/gnu/store")
+   (needed-for-boot? #t)
+   (flags '(no-atime))
+   (options "compress=zstd,subvol=@gnu/store,ssd")))
+
+(define gnu-persist
+  (file-system
+   (device guix-part)
+   (type "btrfs")
+   (mount-point "/gnu/persist")
+   (needed-for-boot? #t)
+   (flags '(no-atime))
+   (options"subvol=@gnu/persist,ssd")))
+
+(define gnu-persist-ssh
+  (file-system
+   (device "/gnu/persist/etc/ssh")
+   (type "none")
+   (mount-point "/etc/ssh")
+   (flags '(no-atime bind-mount))))
+
+(define gnu-persist-guix
+  (file-system
+   (device "/gnu/persist/etc/guix")
+   (type "none")
+   (mount-point "/etc/guix")
+   (flags '(no-atime bind-mount))))
+
+(define gnu-persist-wireguard
+  (file-system
+   (device "/gnu/persist/etc/wireguard")
+   (type "none")
+   (mount-point "/etc/wireguard")
+   (flags '(no-atime bind-mount))))
+
+
+(define %btrfs-ephemeral-file-systems
   (cons* root
          home
+	 root-user
          boot
+	 boot-efi
          tmp
          run
          var-run
+	 var-log
+	 var-lib
+	 var-guix
+	 gnu-store
+	 gnu-persist
+	 gnu-persist-ssh
+	 gnu-persist-guix
+	 gnu-persist-wireguard
          (delete %debug-file-system
                  %base-file-systems)))
 
