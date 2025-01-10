b594c36374360de4fe5d9d6c2c6ddc37c586e402
Paul Jewell <paul@teulu.org>
2025-01-10 20:58:48 +0000

    Added configuration for venus - x270 laptop.

---
+++ paulj/systems/venus/file-systems.scm
@@ -0,0 +1,57 @@
+(define-module (paulj systems venus file-systems)
+  #:use-module (gnu system file-systems)
+  #:export (%btrfs-file-systems))
+
+(define guix-pool-part
+  (file-system-label "guix-pool"))
+  
+(define guix-boot-part
+  (file-system-label "guix-boot"))
+
+(define root
+  (file-system
+   (device guix-pool-part)
+   (mount-point "/")
+   (type "btrfs")
+   (options "subvol=@root,ssd")))
+
+(define gnu
+  (file-system
+   (device guix-pool-part)
+   (mount-point "/gnu")
+   (type "btrfs")
+   (flags '(no-atime))
+   (needed-for-boot? #t)
+   (options "subvol=@gnu,compress=zstd,ssd")))
+
+(define var-log
+  (file-system
+   (device guix-pool-part)
+   (mount-point "/var/log")
+   (type "btrfs")
+   (flags '(no-atime))
+   (needed-for-boot? #t)
+   (options "subvol=@log,compress=zstd,ssd")))
+
+(define home
+  (file-system
+   (device guix-pool-part)
+   (mount-point "/home")
+   (type "btrfs")
+   (options "subvol=@home")))
+
+(define boot
+  (file-system
+   (device guix-boot-part)
+   (mount-point "/boot/efi")
+   (type "vfat")))
+
+(define %btrfs-file-systems
+  (cons* root
+         gnu
+         var-log
+         home
+         boot
+         (delete %debug-file-system
+                 %base-file-systems)))
+
