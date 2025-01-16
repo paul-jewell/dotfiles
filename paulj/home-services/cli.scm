14b49d10e5a51f361be5e0548f8d02cbc5d2f2a4
Paul Jewell <paul@teulu.org>
2025-01-16 16:24:43 +0000

    added khal

--- paulj/home-services/cli.scm
+++ paulj/home-services/cli.scm
@@ -3,10 +3,12 @@
 (define-module (paulj home-services cli)
   #:use-module (gnu home services)
   #:use-module (gnu packages rust-apps)
+  #:use-module (gnu packages calendar)
   #:export (home-cli-service-type))
 
 (define (home-cli-profile-service config)
   (list ripgrep
+        khal
         bat))
 
 (define home-cli-service-type
