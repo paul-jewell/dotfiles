aa4a7b8deae45179acb9292b5714109e0bb1a118
Paul Jewell <paul@teulu.org>
2025-01-29 09:08:52 +0000

    Updated guix home configuration (upstream changes)

--- paulj/home-services/common.scm
+++ paulj/home-services/common.scm
@@ -17,84 +17,83 @@
   #:use-module (guix gexp))
 
 (define-public common-home-services
-  (list
-   ;; Set environment variables for every session
-   (simple-service 'profile-env-vars-service
-                   home-environment-variables-service-type
-                   '( ;; sort hidden (dot) files first in `ls` listings
-                     ("LC_COLLATE" . "C")
+  (append (list
+           ;; Set environment variables for every session
+           (simple-service 'profile-env-vars-service
+                           home-environment-variables-service-type
+                           '( ;; sort hidden (dot) files first in `ls` listings
+                             ("LC_COLLATE" . "C")
 
-                     ;; emacs is our editor
-                     ("VISUAL" . "emacsclient")
-                     ("EDITOR" . "emacsclient")
+                             ;; emacs is our editor
+                             ("VISUAL" . "emacsclient")
+                             ("EDITOR" . "emacsclient")
 
-                     ;; Add some local directories to $PATH
-                     ("PATH" . "$HOME/.bin:$HOME/.cargo/bin:$PATH")
+                             ;; Add some local directories to $PATH
+                             ("PATH" . "$HOME/.bin:$HOME/.cargo/bin:$PATH")
 
-                     ;; Set Wayand-specific environment variables (taken from RDE)
-                     ("XDG_CURRENT_DESKTOP" . "sway")
-                     ("XDG_SESSION_TYPE" . "wayland")
-                     ("RTC_USE_PIPEWIRE" . "true")
-                     ("SDL_VIDEODRIVER" . "wayland")
-                     ("MOZ_ENABLE_WAYLAND" . "1")
-                     ("CLUTTER_BACKEND" . "wayland")
-                     ("ELM_ENGINE" . "wayland_egl")
-                     ("ECORE_EVAS_ENGINE" . "wayland-egl")
-                     ("QT_QPA_PLATFORM" . "wayland-egl")))
+                             ;; Set Wayand-specific environment variables (taken from RDE)
+                             ("XDG_CURRENT_DESKTOP" . "sway")
+                             ("XDG_SESSION_TYPE" . "wayland")
+                             ("RTC_USE_PIPEWIRE" . "true")
+                             ("SDL_VIDEODRIVER" . "wayland")
+                             ("MOZ_ENABLE_WAYLAND" . "1")
+                             ("CLUTTER_BACKEND" . "wayland")
+                             ("ELM_ENGINE" . "wayland_egl")
+                             ("ECORE_EVAS_ENGINE" . "wayland-egl")
+                             ("QT_QPA_PLATFORM" . "wayland-egl")))
 
-   ;; Set up the shell environment
-   (service home-bash-service-type
-            (home-bash-configuration
-             (bash-profile
-              `(,(plain-file "bash-sway-login"
-                             (string-append
-                              "if [ -z \"WAYLAND_DISPLAY\" ] && [ \"$XDG_VTNR\" -eq 1 ]; then\n"
-                              "  exec sway\n"
-                              "fi\n"))))
-             (bashrc
-              `(,(local-file "../files/bash-prompt")))))
+           ;; Set up the shell environment
+           (service home-bash-service-type
+                    (home-bash-configuration
+                     (bash-profile
+                      `(,(plain-file "bash-sway-login"
+                                     (string-append
+                                      "if [ -z \"WAYLAND_DISPLAY\" ] && [ \"$XDG_VTNR\" -eq 1 ]; then\n"
+                                      "  exec sway\n"
+                                      "fi\n"))))
+                     (bashrc
+                      `(,(local-file "../files/bash-prompt")))))
 
-   ;; Place other files
-   (simple-service 'profile-files-service
-                   home-files-service-type
-                   (list `(".inputrc" ,(local-file "../files/inputrc"))))
-      ;; GnuPG configuration
-   (service home-gpg-agent-service-type
-            (home-gpg-agent-configuration
-             (pinentry-program
-              (file-append pinentry-emacs "/bin/pinentry-emacs"))
-             (ssh-support? #t)
-             (default-cache-ttl 28800)
-             (max-cache-ttl 28800)
-             (default-cache-ttl-ssh 28800)
-             (max-cache-ttl-ssh 28800)))
+           ;; Place other files
+           (simple-service 'profile-files-service
+                           home-files-service-type
+                           (list `(".inputrc" ,(local-file "../files/inputrc"))))
+           ;; GnuPG configuration
+           (service home-gpg-agent-service-type
+                    (home-gpg-agent-configuration
+                     (pinentry-program
+                      (file-append pinentry-emacs "/bin/pinentry-emacs"))
+                     (ssh-support? #t)
+                     (default-cache-ttl 28800)
+                     (max-cache-ttl 28800)
+                     (default-cache-ttl-ssh 28800)
+                     (max-cache-ttl-ssh 28800)))
 
-   ;; Emacs configuration
-   (service home-emacs-config-service-type)
+           ;; Emacs configuration
+           (service home-emacs-config-service-type)
 
-   ;; Run user dbus session
-   (service home-dbus-service-type)
+           ;; Run user dbus session
+           (service home-dbus-service-type)
 
 
-   ;; Start background jobs
-   (service home-mcron-service-type
-            (home-mcron-configuration
-             (jobs
-              (list
-               #~(job
-                  '(next-hour (range 0 24 4))
-                  "~/.dotfiles/.bin/sync-passwords")))))
+           ;; Start background jobs
+           (service home-mcron-service-type
+                    (home-mcron-configuration
+                     (jobs
+                      (list
+                       #~(job
+                          '(next-hour (range 0 24 4))
+                          "~/.dotfiles/.bin/sync-passwords")))))
 
-   ;; cli applications
-   (service home-cli-service-type)
-   
-   ;; File synchronization
-   (service home-syncthing-service-type)
+           ;; cli applications
+           (service home-cli-service-type)
+           
+           ;; File synchronization
+           (service home-syncthing-service-type)
 
-   ;; Monitor battery levels
-   (service home-batsignal-service-type)
-   
-   ;; Udiskie for auto-mounting devices
-   (service home-udiskie-service-type)))
-  
-  
+           ;; Monitor battery levels
+           (service home-batsignal-service-type)
+           
+           ;; Udiskie for auto-mounting devices
+           (service home-udiskie-service-type))
+          %base-home-services))
